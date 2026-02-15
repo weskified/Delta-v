65b1415945c5c153bf9c68deecc21e30f6bbff70
wesker <seanderscore@gmail.com>
2026-02-15 19:32:44 +0800

    i think thats cool

--- Content.Server/_DV/Speech/EntitySystems/AustralianAccentSystem.cs
+++ Content.Server/_DV/Speech/EntitySystems/AustralianAccentSystem.cs
@@ -19,7 +19,7 @@ public sealed class AustralianAccentSystem : EntitySystem
                 "friend", "pal", "buddy", "dude", "man", "guy",
                 "bro", "brother", "sir", "mister", "boss"
             },
-            "mate"
+            new[] { "mate", "cobba" }
         );
 
         _st.AddRule(
@@ -27,10 +27,13 @@ public sealed class AustralianAccentSystem : EntitySystem
             new[] { "sheila", "missus" }
         );
 
-        _st.AddRule(new[] { "engineer", "engi" }, "sparkie");
-        _st.AddRule(new[] { "security", "sec", "secoff", "sec off" }, new[] { "coppa", "copper", "walloper" });
-        _st.AddRule(new[] { "courier", "mailman" }, "postie");
-        _st.AddRule("janitor", "garbo");
+        _st.AddRule(new[] { "doctor", "medical", "physician" }, "doc", probability: 0.7);
+        _st.AddRule(new[] { "paramedic", "medical doctor", "medic" }, "ambo", probability: 0.5);
+        _st.AddRule(new[] { "scientist", "researcher" }, "boffin", probability: 0.7);
+        _st.AddRule(new[] { "cargo", "quartermaster" }, "truckie", probability: 0.7);
+        _st.AddRule(new[] { "botanist", "gardener" }, "greenie", probability: 0.7);
+        _st.AddRule("clown", "funny bugger", probability: 0.7);
+        _st.AddRule(new[] { "syndicate", "traitor", "nuke op", "nuclear operative" }, "shonky bastard", probability: 0.7);
 
         // gen vocab / expressions
         _st.AddRule(new[] { "hello", "hi" }, "g'day", applyOnce: true);
@@ -51,6 +54,11 @@ public sealed class AustralianAccentSystem : EntitySystem
         _st.AddRule("fucking", "farking");
         _st.AddRule("hell", "'ell");
         _st.AddRule("shit", "shite");
+        _st.AddRule("idiot", new[] { "drongo", "galah", "muppet" });
+        _st.AddRule(new[] { "no problem", "you're welcome", "its okay" }, "no worries");
+        _st.AddRule("beer", new[] { "liquid gold" });
+        _st.AddRule("wine", "goon"); //blame JohnHelldiver for this
+        _st.AddRule("drunk", "maggot");
 
         // slangs
         _st.AddRule("afternoon", "arvo");
@@ -58,14 +66,9 @@ public sealed class AustralianAccentSystem : EntitySystem
         _st.AddRule("chocolate", "chocky");
         _st.AddRule("mosquito", "mozzie");
         _st.AddRule("present", "prezzy");
-
-        _st.AddRule(
-            "drinking",
-            new[] { "sinking a few", "sinking" },
-            probability: 0.50
-        );
-
-        _st.AddRule(new[] { "cig", "cigg", "cigarette" }, "ciggy");
+        _st.AddRule("kangaroo", new[] { "roo", "joey" });
+        _st.AddRule(new[] { "candy", "sweets" }, "lollies");
+        _st.AddRule(new[] { "cig", "cigg", "cigarette" }, new[] { "ciggy", "dart" });
 
         // phonetic stuff
         _st.AddRule(@"ight\b", "oight", position: MatchPosition.Nothing);
@@ -77,6 +80,10 @@ public sealed class AustralianAccentSystem : EntitySystem
         _st.AddRule(@"ar\b", "ah", position: MatchPosition.Nothing);
         _st.AddRule(@"(?<=[aeiou])r\b", "", position: MatchPosition.Nothing);
         _st.AddRule(@"ing\b", "in'", position: MatchPosition.Nothing);
+        _st.AddRule(@"ay", "ai", probability: 0.30, position: MatchPosition.Nothing);
+        _st.AddRule(@"rry\b", "zza", probability: 0.50, position: MatchPosition.Nothing);
+        _st.AddRule(@"\bto\b", "ta", position: MatchPosition.Nothing);
+        _st.AddRule(@"\band\b", "n'", position: MatchPosition.Nothing);
 
         SubscribeLocalEvent<AustralianAccentComponent, AccentGetEvent>(OnAccent);
     }
